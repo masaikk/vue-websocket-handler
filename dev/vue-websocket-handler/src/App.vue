f4b992e9ad5a2bdc3cb92cea3b1bb734ea4c6e29
masaikk <201830660420@mail.scut.edu.cn>
2023-01-31 10:30:05 +0800

    add dev

---
+++ dev/vue-websocket-handler/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="vueWebsocketHandler" />
+</template>
+
+<style scoped>
+</style>
