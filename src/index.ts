e38232dd3cc534b89d0a294da64f73845b79c573
masaikk <jianqiaohu2000@163.com>
2023-02-05 16:43:04 +0800

    feature: fix README

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,6 @@
-import type { WebSocketConfig, HandlerEmitter } from "./types";
-import { checkWindowWebSocket, fixUrl } from "./utils";
+import { useWebsocket, useWebSocketPlugin } from "./lib";
+import type { WebSocketConfig } from "./lib";
 
-const useWebSocketPlugin = {
-  install(app: any) {
-    app.config.globalProperties.$ws = this;
-  },
-};
+export { useWebSocketPlugin, useWebsocket };
 
-const useWebsocket = (config?: WebSocketConfig) => {
-  if (!checkWindowWebSocket()) {
-    console.error("Your browser doesn't support websocket");
-    throw new Error("no support");
-  }
-  const configHost: string = config?.host as string;
-  let port: string = JSON.stringify(config?.port);
-  if (port[0] == '"') {
-    port = port.slice(1, port.length - 1);
-  }
-  const formattedUrl: string = fixUrl(config?.url);
-  const protocol: string = `ws://${configHost}:${port}${formattedUrl}`;
-  let thisWebSocket: WebSocket = new WebSocket(protocol);
-  thisWebSocket.onopen = () => {
-    console.log(`successful setup WebSocket at ${protocol}`);
-  };
-  thisWebSocket.onerror = () => {
-    console.error(`error`);
-  };
-  const emitters: HandlerEmitter[] = [];
-  if (config?.emitters as HandlerEmitter) {
-    emitters.push(config?.emitters as HandlerEmitter);
-  } else {
-    if (typeof config?.emitters !== "undefined") {
-      (config?.emitters as HandlerEmitter[]).map((emitter) => {
-        emitters.push(emitter);
-      });
-    }
-  }
-  const timeout: number =
-    typeof config?.timeout === "undefined" ? 5000 : config?.timeout;
-
-  return {
-    // client
-    client: thisWebSocket,
-
-    // log version in console
-    logVersion: () => {
-      console.log("Version 0.0.0");
-      console.log(thisWebSocket);
-    },
-    emitters,
-  };
-};
-
-export { useWebsocket, useWebSocketPlugin };
 export type { WebSocketConfig };
